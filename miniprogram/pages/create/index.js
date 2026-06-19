91d082be4286956aa1b1c559491faf5a7cd32768
zywwei <1042377941@qq.com>
2026-06-19 22:19:59 +0800

    fix: 奇趣屋入口页改为标准导航栏，跟随主题颜色

--- miniprogram/pages/create/index.js
+++ miniprogram/pages/create/index.js
@@ -6,15 +6,26 @@ Page({
 
   onLoad: function() {
     this.loadStats()
+    this.setThemeColor()
   },
 
   onShow: function() {
     this.loadStats()
+    this.setThemeColor()
 
     // 更新 tabBar 选中状态
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
       this.getTabBar().setData({ selected: 4 })
     }
+  },
+
+  setThemeColor: function() {
+    var app = getApp()
+    wx.setNavigationBarColor({
+      frontColor: '#ffffff',
+      backgroundColor: app.globalData.themeColor || '#FF9AAB',
+      animation: { duration: 0 }
+    })
   },
 
   loadStats: function() {
