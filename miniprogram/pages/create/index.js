7641699fcf37212e8c1bbbdf1cf4afa5c35190dc
zywwei <1042377941@qq.com>
2026-06-25 14:29:17 +0800

    fix: 修复主包 create/index.js 的 require 路径
    主包页面应使用相对路径（../../utils/xxx.js），不能使用绝对路径（/utils/xxx.js）

--- miniprogram/pages/create/index.js
+++ miniprogram/pages/create/index.js
@@ -1,5 +1,5 @@
-var childStorage = require('/utils/child-storage.js')
-var aiManager = require('/packageCreate/utils/ai-manager.js')
+var childStorage = require('../../utils/child-storage.js')
+var aiManager = require('../../packageCreate/utils/ai-manager.js')
 
 Page({
   data: {
